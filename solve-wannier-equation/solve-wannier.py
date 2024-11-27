d12ccb5cb6d728911819a988f892a8f3e266a68b
Pol <polbeco@gmail.com>
2024-11-27 12:03:58 +0000

    parallelization implemented for Wannier matrix construction, format of the all code changed slightly

--- solve-wannier-equation/solve-wannier.py
+++ solve-wannier-equation/solve-wannier.py
@@ -2,15 +2,18 @@
 # University of Cambridge and Universitat Politècnica de Catalunya
 
 # This code solves the Wannier Equation for a given 2d material monolayer and provide
-# the binding energies and excitonic-wave functions
+# the binding energies and excitonic wavefunctions
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-# functions
-def compute_wannier_matrix(m_r, k_mesh, k_step, dielec_layer, dielec_out, l_quantum_number, thick_layer, theta_mesh, theta_step):
+from multiprocessing import Pool
+
+
+############################### FUNCTIONS ##############################
+def compute_wannier_matrix_parallel(m_r, k_mesh, k_step, dielec_layer, dielec_out, l_quantum_number, thick_layer, theta_mesh, theta_step):
     """
-    It determines the Wannier matrix of the Wannier equation
+    Computes the Wannier matrix in parallel
 
     Inputs:
         m_r -> relative mass of the exciton system
@@ -25,9 +28,71 @@ def compute_wannier_matrix(m_r, k_mesh, k_step, dielec_layer, dielec_out, l_quan
     """
 
     w_ij = np.zeros((len(k_mesh), len(k_mesh)), dtype=complex)
-    hbar = 0.6582 # eV·fs
-    electron_charge = 1 # eV
-    vacuum_permittivity = 0.05526 # nm/fs
+
+    tasks = [
+        (
+            i_index, j_index, k_mesh, k_step, dielec_layer, dielec_out, 
+            l_quantum_number, thick_layer, theta_mesh, theta_step, m_r
+        )
+        for i_index in range(len(k_mesh))
+        for j_index in range(len(k_mesh))
+    ]
+
+    with Pool() as pool:
+        results = pool.map(compute_single_element, tasks)
+
+    for i_index, j_index, w_ij_value in results:
+        w_ij[i_index, j_index] = w_ij_value
+
+    return w_ij
+
+
+def compute_single_element(args):
+    """
+    Computes a single element of the Wannier matrix w_ij, for parallelization of compute_wannier_matrix_parallel
+    function, using multiprocessing
+
+    Inputs:
+        args -> the arguments that compute_wannier_matrix_parallel recives (are defined below)
+    """
+
+    i_index, j_index, k_mesh, k_step, dielec_layer, dielec_out, l_quantum_number, thick_layer, theta_mesh, theta_step, m_r = args
+
+    V_coul = 0
+    for theta_index in range(len(theta_mesh)):
+        if (i_index == j_index) and (theta_index == 0):
+            continue
+        else:
+            q_element = np.sqrt((k_mesh[i_index]**2) + (k_mesh[j_index]**2) - (2 * k_mesh[i_index] * k_mesh[j_index] * np.cos(theta_mesh[theta_index])))
+            V_coul = V_coul + pot_rt(q_element, dielec_layer, dielec_out, thick_layer, l_quantum_number, theta_mesh[theta_index])
+
+    V_coul = V_coul * ((electron_charge**2) / (((2 * np.pi)**2) * vacuum_permittivity * dielec_out)) * k_mesh[j_index] * k_step * theta_step
+
+    w_ij_value = -V_coul
+
+    if i_index == j_index:
+        w_ij_value = w_ij_value + (((hbar**2) * (k_mesh[i_index]**2)) / (2 * m_r))
+
+    return i_index, j_index, w_ij_value
+
+
+def compute_wannier_matrix(m_r, k_mesh, k_step, dielec_layer, dielec_out, l_quantum_number, thick_layer, theta_mesh, theta_step):
+    """
+    [DEPRECATED, but functional] It determines the Wannier matrix of the Wannier equation (without parallelization)
+
+    Inputs:
+        m_r -> relative mass of the exciton system
+        k_mesh -> array with the mesh of the momentum space
+        k_step -> step in the k_mesh (homogeneous mesh assumed)
+        dielec_layer -> dielectric constant of the monolayer
+        dielec_out -> dielectric constant of the surroundings of the layer
+        l_quantum_number -> the angular quantum number of the excitonic system (L=0: s, L=1: p, ...)
+        thick_layer -> thickness of the monolayer
+        theta_mesh -> array with the mesh of the theta angle
+        theta_step -> step in the theta_mesh (homogeneous mesh assumed)
+    """
+
+    w_ij = np.zeros((len(k_mesh), len(k_mesh)), dtype=complex)
 
     for i_index in range(len(k_mesh)):
         for j_index in range(len(k_mesh)):
@@ -47,7 +112,6 @@ def compute_wannier_matrix(m_r, k_mesh, k_step, dielec_layer, dielec_out, l_quan
                 w_ij[i_index, j_index] = w_ij[i_index, j_index] + (((hbar**2) * (k_mesh[i_index]**2)) / (2 * m_r)) 
 
     return w_ij
-
 
 
 def pot_rt(q_element, dielec_layer, dielec_out, thick_layer, l_quantum_number, theta_i):
@@ -91,6 +155,7 @@ def diagonalize_matrix(matrix):
 
     return sorted_eigenvalues, sorted_eigenvectors
 
+
 def normalize_eigenvectors(eigenvectors, k_mesh):
     """
     Normalize the eigenvectors (wavefunctions)
@@ -113,37 +178,59 @@ def normalize_eigenvectors(eigenvectors, k_mesh):
         norm_eigenvectors.append(norm_eigen)
 
     return norm_eigenvectors
+########################################################################
 
-# results
+
+############################### CONSTANTS ##############################
+# Physical constants
+hbar = 0.6582 # eV·fs
+electron_charge = 1 # eV
+vacuum_permittivity = 0.05526 # nm/fs
+
+# Case specific constants
 relative_mass = 0.108 * 5.68568 # fs^2eV/nm^2
-dielectric_layer = 3.32
-dielectric_outside = 6.1
-l_quantum_number = 0
+dielectric_layer = 3.32 # adimensional
+dielectric_outside = 6.1 # adimensional
 thickness_layer = 0.636 # nm
 
+# Angular quantum number
+l_quantum_number = 0
+
+# Relative momentum mesh
 k_min = 0.01
 k_max = 5
 number_points_k = 500
 k_mesh = np.linspace(k_min, k_max, number_points_k)
 k_step = (k_max - k_min) / number_points_k
 
+# Theta mesh
 theta_min = 0
 theta_max = 2 * np.pi
 number_points_theta = 250
 theta_mesh = np.linspace(theta_min, theta_max, number_points_theta, endpoint=False)
 theta_step = (theta_mesh[-1] - theta_mesh[0]) / (len(theta_mesh))
-wannier_matrix = compute_wannier_matrix(relative_mass, k_mesh, k_step, dielectric_layer, dielectric_outside, l_quantum_number, thickness_layer, theta_mesh, theta_step)
+########################################################################
+
+
+############################## GET RESULTS #############################
+# Generate Wannier matrix
+wannier_matrix = compute_wannier_matrix_parallel(relative_mass, k_mesh, k_step, dielectric_layer, dielectric_outside, l_quantum_number, thickness_layer, theta_mesh, theta_step)
+
+# Diagonalize the Wannier matrix
 eigenvalues, eigenvectors = diagonalize_matrix(wannier_matrix)
 
-# normalize the eigenvectors
+# Normalize the eigenvectors (excition wavefunctions)
 norm_eigenvectors = normalize_eigenvectors(eigenvectors, k_mesh)
+########################################################################
 
-# print the eigenvalues of the low energy states
+
+############################# SHOW RESULTS #############################
+# Print the eigenvalues of the low energy states
 print('The Exciton Binding Energies are: ')
 for level in range(5):
     print(f'    The exciton level {level + 1}s has a binding energy of: {int(np.real(eigenvalues[level]) * -1000)} meV')
 
-# plot the results
+# Plot the results
 fig, ax = plt.subplots(2, 1, figsize=(4, 5))
 ax[0].set_title('Eigenvalues')
 ax[0].set_ylabel('Exciton binding energy (meV)')
@@ -172,3 +259,4 @@ ax[0].set_xticks(ticks=before, labels=after)
 
 plt.tight_layout()
 plt.savefig('results.pdf')
+########################################################################
